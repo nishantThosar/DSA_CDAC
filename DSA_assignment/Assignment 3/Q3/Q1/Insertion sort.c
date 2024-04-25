2ae9b9dfcd4d3c61d16f29a2c5852942c3da6a85
System_32 <162736264+nishantThosar@users.noreply.github.com>
2024-04-25 08:58:08 +0530

    Update Insertion sort.c

--- DSA_assignment/Assignment 3/Q3/Q1/Insertion sort.c
+++ DSA_assignment/Assignment 3/Q3/Q1/Insertion sort.c
@@ -1,5 +1,6 @@
 //Code for selection sort
 
+#include<stdio.h>
  
 int main()
 {
@@ -11,15 +12,32 @@ int main()
        key = arr[i];
        j = i-1;
  
-       /* Move elements of arr[0..i-1], that are
-          greater than key, to one position ahead
-          of their current position */
-       while (j >= 0 && arr[j] > key)
-       {
-           arr[j+1] = arr[j];
-           j = j-1;
+       /* Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position */
+       /*if j is grater than 0 and element of array is grater than key then only the program enters in while loop*/
+       while (j >= 0 && arr[j] > key){
+           arr[j+1] = arr[j];//To traverse the elements of array.
+           j = j-1;//Insert the next element to the privious element in array.
        }
-       arr[j+1] = key;
+       /*Once all comparision is done the element is inserted at correct place */
+       /*Eg array = [10,40,5,30,15,70]
+	*key = 40, now in first iteration the program compares whole arraay with the key and swaps all the lesser elements than 40 to the \
+       		lh side and keeps all big elements to rh side
+	array = [10,_,_,30,15,70]
+	*key = 40   ^ ^   
+		 vaccant due to swapping.
+	Next step is to insert the key at j+1 place 
+	array = [10,5,40,30,15,70]
+		    ^  ^
+		   Swapped 
+	
+	* */
+       arr[j+1] = key;//Once all the comparision is done then insert the key at place
    }
+
+   //for printing the array
+   for(int i=0; i<6;i++){
+   	printf("%d\n",arr[i]);
+
+   }// for ends
     return 0;
 }
